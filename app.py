2a6809926016d944dd363821d7efffa9772b24b5
yaghdev paliwal <yaghdev.paliwal@york.ac.uk>
2024-02-07 14:48:05 +0000

    write into the database.

--- app.py
+++ app.py
@@ -3,18 +3,40 @@ import flask
 from flask import Flask, request, render_template
 import json
 from transformers import pipeline
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-BART_PATH = 'bart-large'
-
+# create the extension
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///summarization.db"
+# initialize the app with the extension
+db.init_app(app)
 
-summarizer = pipeline("summarization", model="lucadiliello/bart-small")
 
-def bart_summarize(input_text):
+class Summary(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    input = db.Column(db.String)
+    summary = db.Column(db.String)
+    model_name = db.Column(db.String)
+
+with app.app_context():
+    db.create_all()
+
+small_summarizer = pipeline("summarization", model="lucadiliello/bart-small")
+large_summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+def bart_summarize(input_text, model):
     input_text = str(input_text)
     input_text = ' '.join(input_text.split())
+
+    if model == "bart_small":
+        return small_summarizer(input_text, max_length=130, min_length=30, do_sample=False)[0]["summary_text"]
     
-    return summarizer(input_text, max_length=130, min_length=30, do_sample=False)[0]["summary_text"]
+    if model == "bart_large":
+        return large_summarizer(input_text, max_length=130, min_length=30, do_sample=False)[0]["summary_text"]
 
 
 @app.route('/')
@@ -25,16 +47,28 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        sentence = request.json['input_text']
-                 
+        sentence = request.json['input_text']                 
         model = request.json['model']
-        if sentence != '':            
-            output = bart_summarize(sentence)            
-            response = {}
+        if sentence != '':  
+            if Summary.query.filter_by(input=sentence, model_name=model).first():                
+                output = Summary.query.filter_by(input=sentence, model_name=model).first().summary
+            else:
+                output = bart_summarize(sentence, model)  
+                user = Summary(
+                    input=sentence,
+                    summary=str(output),
+                    model_name=str(model)
+                )
+
+                db.session.add(user)
+                db.session.commit()  
+
+            response = {}          
             response['response'] = {
                 'summary': str(output),
                 'model': model.lower()
             }
+
             return flask.jsonify(response)
         else:
             res = dict({'message': 'Empty input'})
